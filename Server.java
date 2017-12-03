3cba091defbe087cac93e29233e9fa666a755a29
Unknown <nestorowicz1@gmail.com>
2017-12-03 18:03:02 +0100

    Stworzenie branch'a i RootHandler
    -serwer wysyła do użytkownika stronę html
    -poprawenie funkcjionalności path do pliku

---
+++ Server.java
@@ -0,0 +1,46 @@
+import com.sun.net.httpserver.*;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+
+//=================================================================================================
+//  Aby odpalic server w consoli musisz wpisac polecenie: java Server
+//  Po wprowadzeniu zmian należy skompilować plik czyli komenda: javac +path+ nazwa pliku z .java
+//================================================================================================
+public class Server {
+    public static void main(String[] args) throws IOException {
+        int port = 9000;
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        System.out.println("server started at " + port);
+        server.createContext("/", new RootHandler());
+//    server.createContext("/echoHeader", new EchoHeaderHandler());
+//    server.createContext("/echoGet", new EchoGetHandler());
+//    server.createContext("/echoPost", new EchoPostHandler());
+        server.setExecutor(null);
+        server.start();
+    }
+}
+class RootHandler implements HttpHandler {
+
+    @Override
+
+    public void handle(HttpExchange he) throws IOException {
+        File file = new File(RootHandler.class.getProtectionDomain().getCodeSource().getLocation().getPath()+"/test.html");
+        byte[] b = new byte[(int) file.length()];
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            fileInputStream.read(b);
+            for (int i = 0; i < b.length; i++) {
+                System.out.print((char)b[i]);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not Found.");
+            e.printStackTrace();
+        }
+        he.sendResponseHeaders(200, file.length());
+        OutputStream os = he.getResponseBody();
+
+        os.write(b);
+        os.close();
+    }
+}
